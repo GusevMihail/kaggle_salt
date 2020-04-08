b3d5ae08c598d472fb401e8788b135dbf38658e5
Mihail <gusev.mihail.34@gmail.com>
2020-04-08 14:47:41 +0300

    update save_model and load_model functions

--- utils.py
+++ utils.py
@@ -12,12 +12,14 @@ def save_model(path, model, optimizer, scheduler, train_history, val_history):
     print('successfully saved')
 
 
-def load_model(path, model, optimizer, scheduler, train_history=None, val_history=None):
+def load_model(path, model, optimizer, scheduler=None, train_history=None, val_history=None):
     checkpoint = torch.load(path)
     model.load_state_dict(checkpoint['model_state_dict'])
     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-    scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
+    if scheduler:
+        scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
     if train_history:
         train_history = checkpoint['train_history']
+    if val_history:
         val_history = checkpoint['val_history']
     print('successfully loaded')
